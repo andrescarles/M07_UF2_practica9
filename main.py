f78a29d25b2d4755cee52d4909edea8b2cb3c984
ivancp54 <icozar21@jaumebalmes.net>
2022-12-15 20:45:39 +0100

    final

---
+++ main.py
@@ -0,0 +1,31 @@
+from exercici1 import *
+from exercici2 import *
+from exercici4_nomAlumne.py import *
+
+print ("Funcion ejercicio1:", funcion1())
+print ("Tamaño ejercicio1:", funcion1().size)
+print ("Dimensiones:", funcion1().ndim)
+print ("Tipo de elemento interno:", funcion1().dtype)
+print("Tamaño de la matriz", funcion1().shape)
+
+print ("Array1:", array1())
+print ("Tamaño array1:", array1().size)
+print ("Dimensiones:", array1().ndim)
+print ("Tipo de elemento interno:", array1().dtype)
+print("Tamaño de la matriz", array1().shape)
+
+
+print ("Array2:", array2())
+print ("Tamaño array2:", array2().size)
+print ("Dimensiones:", array2().ndim)
+print ("Tipo de elemento interno:", array2().dtype)
+print("Tamaño de la matriz", array2().shape)
+
+
+print ("Array3:", array3())
+print ("Tamaño Array3:", array3().size)
+print ("Dimensiones:", array3().ndim)
+print ("Tipo de elemento interno:", array3().dtype)
+print("Tamaño de la matriz", array3().shape)
+
+print(function4())
