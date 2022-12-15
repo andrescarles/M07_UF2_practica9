a885aaf68b553cd84d5e6e7472b294757dfe9006
ivancp54 <icozar21@jaumebalmes.net>
2022-12-15 19:47:45 +0100

    parte Ivan

---
+++ exercici2.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def array1 ():
+    a = np.array([88, 23, 39, 41])
+    return a
+
+def array2 ():
+    b = np.array([[76.4, 21.7, 38.4], [41.2, 52.8, 68.9]])
+    return b
+
+def array3 ():
+    c = np.array([[12],
+                 [4],
+                 [9],
+                 [8]])
+    return c
+
+print (array1())
+print (array2())
+print (array3())
